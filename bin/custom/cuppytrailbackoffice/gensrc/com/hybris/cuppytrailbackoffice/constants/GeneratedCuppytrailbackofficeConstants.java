d1d718ce165fe57da388f6acae20546fd2ab17a2
Edgard Neto <edgardquirino@gmail.com>
2016-10-31 14:56:48 +0700

    Commit only custom config

---
+++ bin/custom/cuppytrailbackoffice/gensrc/com/hybris/cuppytrailbackoffice/constants/GeneratedCuppytrailbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 31, 2016 2:41:22 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.cuppytrailbackoffice.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrailbackoffice";
+	
+	protected GeneratedCuppytrailbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
